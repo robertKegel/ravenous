956f76fee07e35ffa0b5173c7e7be88b9e0e6924
robertKegel <kegel.robert@gmail.com>
2020-09-28 16:43:15 -0400

    added yelp search api

---
+++ src/util/Yelp.js
@@ -0,0 +1,32 @@
+const apiKey = "d_MdtdSFXkWMaa_JqVKVB19ecUgeD8O-gwR_8oTuGL-iy8Bho22F5avkIYHyucuA2XleAD7P4r4iVACKbIVsjGA0hogg02D2HBlOP9kmx3lynWYvxYjKFBm6IPZxX3Yx"
+
+export const Yelp = {
+    search( term, location, sortBy ) {
+        return fetch( `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, { 
+            headers: { Authorization: `Bearer ${apiKey}`}
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => {
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+                        rating: business.rating,
+                        reviewCount: business.review_count,
+                        url: business.url,
+                        latitude: business.coordinates.latitude,
+                        longitude: business.coordinates.longitude
+                    }
+                })
+            }
+        });
+        
+    }
+}
