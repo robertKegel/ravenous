3dc97a915e24d3effc41239263d8f86afa763a0b
robertKegel <kegel.robert@gmail.com>
2020-11-08 22:09:35 -0500

    build for deployment to iamrobertkegel.com

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -2,7 +2,7 @@ const apiKey = "d_MdtdSFXkWMaa_JqVKVB19ecUgeD8O-gwR_8oTuGL-iy8Bho22F5avkIYHyucuA
 
 export const Yelp = {
     search( term, location, sortBy ) {
-        return fetch( `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, { 
+        return fetch( `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, { 
             headers: { Authorization: `Bearer ${apiKey}`}
         }).then(response => {
             return response.json();
