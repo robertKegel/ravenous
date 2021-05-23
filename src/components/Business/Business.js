0b007599e5d8b7434814f7f76c3bc5e9c5181770
robertKegel <kegel.robert@gmail.com>
2021-05-22 23:30:27 -0400

    add rel='noopener noreferrer' to maps link

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -13,14 +13,14 @@ class Business extends React.Component {
         return (
             <div className="Business">
               <div className="image-container">
-                <a target="_blank" href={this.props.business.url}>
+                <a target="_blank" rel="noopener noreferrer" href={this.props.business.url}>
                   <img src={this.props.business.imageSrc} alt=''/>
                 </a>
               </div>
               <h2>{this.props.business.name}</h2>
               <div className="Business-information">
                 <div className="Business-address">
-                  <a target="_blank" href={this.googleMapsUrl}>
+                  <a target="_blank" rel="noopener noreferrer" href={this.googleMapsUrl}>
                     <p>{this.props.business.address}</p>
                     <p>{this.props.business.city}</p>
                     <p>{this.props.business.state} {this.props.business.zipCode}</p>
